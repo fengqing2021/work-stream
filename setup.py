6d4c118062a9155f3a7dfeaddbd1d70a9007de32
fegnqing2021 <fq20210602@gmail.com>
2022-03-19 17:59:07 +0800

    update 修正内容

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
       name='work_stream',
       version='0.0.1',
       author='fengqing',
-      description='工作流',
+      author_email='fq20210602@gmail.com',
+      url="https://github.com/fengqing2021/work-stream",
+      description='这是一个运维工作流模块',
       packages=find_packages(),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       zip_safe=True,
+      python_requires=">=3.6",
 )
