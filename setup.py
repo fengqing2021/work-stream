5c991e4f3b905081367a85d2c6ca07d1045ef1d0
fegnqing2021 <fq20210602@gmail.com>
2022-03-19 13:28:52 +0800

    update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='work_stream',
+      version='0.0.1',
+      author='fengqing',
+      description='工作流',
+      packages=find_packages(),
+      zip_safe=True,
+)
